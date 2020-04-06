a834fa74e4a41689c174e27b5a2a00d169c23609
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-06 18:03:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,6 +5,6 @@ def start_game(player1, player2)
   puts "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
